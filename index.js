af90e738c48d17e9df3ebba26002904aac844ce9
emags112 <magnussen.evan@gmail.com>
2018-12-23 17:02:46 -0500

    added pure styling

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const   express =   require('express'),
-        request =   require('request'),
-        app     =   express(),
-        apiKey = '&api_key=Y2twmHii37VedsrgffYyooeLLobd4WOBPvEVW5zg',
-        apiBaseURL  = 'https://api.nps.gov/api/v1';
+const   express     =   require('express'),
+        request     =   require('request'),
+        bodyParser  =   require('body-parser'),
+        app         =   express(),
+        apiKey      =   '&api_key=Y2twmHii37VedsrgffYyooeLLobd4WOBPvEVW5zg',
+        apiBaseURL  =   'https://api.nps.gov/api/v1';
+        
+
+let data = false;
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', function(req, res){
-    res.render('home');
+    res.render('home', {data: data});
+})
+
+app.post('/', function(req, res){
+    let dataSet = req.body.searchData;
+    let search = '/' + dataSet;
+    res.redirect(search);
 })
 
 app.get('/:dataSet', function(req, res){
