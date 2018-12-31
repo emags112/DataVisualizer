963859ae5a50eca9b1dfbc1172182d2350c67049
emags112 <magnussen.evan@gmail.com>
2018-12-31 12:58:21 -0500

    weather cards added and formatted

--- alphs.js
+++ alphs.js
@@ -6,12 +6,12 @@ const   express     =   require('express'),
         app         =   express(),
         apiKey      =   '&api_key=Y2twmHii37VedsrgffYyooeLLobd4WOBPvEVW5zg',
         apiBaseURL  =   'https://api.nps.gov/api/v1',
-        owmAPIKey   =   'f484f0da63efa805be72b9b2bae63cb7',
-        owmAPIBase  =   'https://api.openweathermap.org/data/2.5/forecast/daily?',
+        owmAPIKey   =   '',
+        owmAPIBase  =   'http://api.openweathermap.org/data/2.5/weather?',
         lat         =   '32.20909636',
         long        =   '-110.7574974';
 
-request(owmAPIBase + 'lat=' + lat + '&lon=' + long + '&cnt=5&appid=' + owmAPIKey, function (error, response, body) {
+request(owmAPIBase + 'lat=' + lat + '&lon=' + long + '&cnt=10&APPID=f484f0da63efa805be72b9b2bae63cb7', function (error, response, body) {
     if(error){
         res.send(error);
     } else {
--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const   express     =   require('express'),
         app         =   express(),
         apiKey      =   '&api_key=Y2twmHii37VedsrgffYyooeLLobd4WOBPvEVW5zg',
         apiBaseURL  =   'https://api.nps.gov/api/v1';
+        googleKey   =   '&key=AIzaSyAa3dAZiSJoun0zZ4kFmd-LkmqjmRCLFn8'
         owmAPIKey   =   '3d85baf9ef44e792b29549e4b607e8c7'
-        owmAPIBase  =   'https://api.openweathermap.org/data/2.5/forecast/daily?'
+        owmAPIBase  =   'http://api.openweathermap.org/data/2.5/weather?'
         
 
 let data = false,
@@ -30,13 +31,6 @@ app.get('/', function(req, res){
             articles = JSON.parse(body);
         }
     });
-    // request(apiBaseURL + '/alerts?limit=5' + apiKey, function (error, response, body) {
-    //     if(error){
-    //         res.send(error);
-    //     } else {
-    //         alerts = JSON.parse(body);
-    //     }
-    // });
     res.render('home', {articles: articles});
 })
 
@@ -79,8 +73,9 @@ app.get('/park/:parkCode', function(req, res){
             let parks = JSON.parse(body);
             let rand = Math.floor(Math.random()*(parks['data'][0]['images'].length)),
                 randImage = parks['data'][0]['images'][rand]['url'],
-                lat = parks['data'][0]['latLong'].slice(4, 15),
-                long = parks['data'][0]['latLong'].slice(23, 35);
+                lat = parseFloat(parks['data'][0]['latLong'].slice(4, 10)),
+                long = parseFloat(parks['data'][0]['latLong'].slice(21, 30));
+            console.log(parks['data'][0]['latLong']);
             // search for visitor centers
             request(apiBaseURL + '/visitorcenters?parkCode=' + parkCode + '&fields=name,operatingHours,addresses,contacts' + apiKey, function (error, response, body) {
                 if(error){
@@ -94,12 +89,13 @@ app.get('/park/:parkCode', function(req, res){
                         } else {
                             let alerts = JSON.parse(body);
                             // get weather info
-                            request(owmAPIBase + 'lat=' + lat + '&lon=' + long + '&cnt=5&appid=' + owmAPIKey, function (error, response, body) {
+                            request(owmAPIBase + 'lat=' + lat + '&lon=' + long + '&units=imperial&appid=' + owmAPIKey, function (error, response, body) {
                                 if(error){
                                     res.send(error);
                                 } else {
                                     let weather = JSON.parse(body);
-                                    res.render('park', {parks: parks, randImage: randImage, visitorCenter: visitorCenter, alerts: alerts, weather: weather});
+                                    console.log(weather);
+                                    res.render('park', {parks: parks, randImage: randImage, visitorCenter: visitorCenter, alerts: alerts, weather: weather, googleKey: googleKey});
                                 }
                             })
                         }
