f70c771f5057bc40a6afa7573e14a2f294091fe4
emags112 <magnussen.evan@gmail.com>
2019-01-25 21:36:47 -0500

    set to deploy

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ const   express     = require("express"),
         request = require("request"),
         bodyParser = require("body-parser"),
         router = express.Router(),
-        apiKey = process.env.NPSKEY,
-        apiBaseURL = "https://api.nps.gov/api/v1",
+        apiKey = process.env.NPSKEY ,
+        apiBaseURL = "https://developer.nps.gov/api/v1",
         googleKey = process.env.GOOGLEKEY,
         owmAPIKey = process.env.OWMKEY,
         owmAPIBase = "http://api.openweathermap.org/data/2.5/weather?";
 
 router.get("/", function(req, res) {
-  request(apiBaseURL + "/articles?limit=5" + apiKey, function(
+  request(apiBaseURL + "/articles?limit=5&api_key=" + apiKey, function(
     error,
     response,
     body
